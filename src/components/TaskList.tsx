b5b3419f4850f619c9874f42b6409df675084955
Gabriel Rossales <gabriel.rossales@gmail.com>
2021-07-09 00:35:28 -0300

    Handle task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,19 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle){
+      setTasks([...tasks, {id: Math.floor(Math.random()*1000),title: newTaskTitle, isComplete:false}])
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map(task=> (task.id !== id? task: {...task, isComplete: !task.isComplete} )))
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id !== id));
   }
 
   return (
